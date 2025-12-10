245c3e9cc570ca8493c7658dfe96de34a221acf4
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-12-10 17:02:58 +0200

    Entertainments

---
+++ Entertainments.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << min(1000, n*200);
+    return 0;
+}
